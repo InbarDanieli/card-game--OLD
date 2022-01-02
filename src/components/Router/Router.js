4a195959daae301d8749a248715c7b54c10c8e65
Inbar Danieli <inbar0308@gmail.com>
2022-01-02 12:50:29 +0200

    Added-grid

--- src/components/Router/Router.js
+++ src/components/Router/Router.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import AboutPage from '../../aboutpage/AboutPage';
+import AboutPage from '../aboutpage/AboutPage';
 import Game from '../game-page/game/Game';
 import HomePage from '../HomePage/HomePage';
 
